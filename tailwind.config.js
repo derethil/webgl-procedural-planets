ffbd5ae07ef032b18a3d65a4d13daf9894b06bbc
Jaren Glenn <jarenglenn@gmail.com>
2023-04-20 17:40:56 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "forest",
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import postcss from "./postcss.config.js";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+  plugins: [svelte()],
+  css: {
+    postcss,
+  },
+});

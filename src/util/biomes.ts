c910c6342398c1232cf63c2f66a1386fa07b299d
Jaren Glenn <jarenglenn@gmail.com>
2025-08-24 20:06:05 -0600

    feat(biomes): barren biomes to begin with

--- src/util/biomes.ts
+++ src/util/biomes.ts
@@ -22,12 +22,24 @@ export interface Biome {
   color: ColorArray;
 }
 
-export type BiomeType = "Ocean" | "Sand" | "Grass" | "Stone" | "Snow";
+export enum Biomes {
+  Ocean = "Ocean",
+  BarrenFlat = "BarrenFlat",
+  BarrenHill = "BarrenHill",
+  BarrenMountain = "BarrenMountain",
+  Sand = "Sand",
+  Grass = "Grass",
+  Stone = "Stone",
+  Snow = "Snow",
+}
 
-export const Biomes: Record<BiomeType, Biome> = {
-  Ocean: { color: hexToRGBFloatArray("#4287f5") },
-  Sand: { color: hexToRGBFloatArray("#feffae") },
-  Grass: { color: hexToRGBFloatArray("#31703a") },
-  Stone: { color: hexToRGBFloatArray("#83819c") },
-  Snow: { color: hexToRGBFloatArray("#ffffff") },
+export const BiomeColors: Record<Biomes, Biome> = {
+  [Biomes.Ocean]: { color: hexToRGBFloatArray("#4287f5") },
+  [Biomes.BarrenFlat]: { color: hexToRGBFloatArray("#8b7355") },
+  [Biomes.BarrenHill]: { color: hexToRGBFloatArray("#9d8566") },
+  [Biomes.BarrenMountain]: { color: hexToRGBFloatArray("#6b5d45") },
+  [Biomes.Sand]: { color: hexToRGBFloatArray("#feffae") },
+  [Biomes.Grass]: { color: hexToRGBFloatArray("#31703a") },
+  [Biomes.Stone]: { color: hexToRGBFloatArray("#83819c") },
+  [Biomes.Snow]: { color: hexToRGBFloatArray("#ffffff") },
 };

9ff262fcbc03468bda4710857abbf675bfe08ac8
Jaren Glenn <jarenglenn@gmail.com>
2025-09-05 14:14:31 -0600

    feat(ui): add debug inputs for planet params

--- src/ui/PlanetConfigForm.svelte
+++ src/ui/PlanetConfigForm.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+import { planetParams } from "@/state/planetParams.svelte";
   import { newPlanet } from "@/state/planetParams.svelte";
   import { sceneParams } from "@/state/sceneParams.svelte";
 </script>
 
-<div class="m-4">
+<div class="m-4 flex flex-col gap-2">
   <div class="px-1 py-2">
     <button
       class="btn btn-ghost btn-outline btn-sm"
@@ -22,4 +23,31 @@
       onclick={() => (sceneParams.autorotate = !sceneParams.autorotate)}
     />
   </label>
+
+  <label class="label cursor-pointer">
+    <span class="label-text">Radius</span>
+    <input
+      type="range"
+      class="range w-full"
+      min={1}
+      max={50}
+      value={planetParams.radius}
+      onchange={(e) => (planetParams.radius = +(e.target as HTMLInputElement).value)}
+    />
+  </label>
+
+
+  <label class="label cursor-pointer">
+    <span class="label-text">Divisions</span>
+    <input
+      type="range"
+      class="range w-full"
+      min={10}
+      max={50}
+      value={planetParams.divisions}
+      onchange={(e) => (planetParams.divisions = +(e.target as HTMLInputElement).value)}
+    />
+  </label>
+
+
 </div>
